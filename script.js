4b1a01ec9fce4e2d4b274a30d9eb10f9781fbcbc
ariel <ariel29111978@hotmail.com>
2021-04-13 10:40:01 -0300

    modificaciones varias y se agrego el script funcionando

---
+++ script.js
@@ -0,0 +1,32 @@
+const acordion = document.getElementById('main__list');
+const title = document.querySelectorAll('.main__list-title')
+const text = document.querySelectorAll('.main__list-text-container');
+const arrow = document.querySelectorAll('.main__img-arrow')
+
+const desactivar = targetElement => {
+    for (i of title){
+        i.classList.remove('main__list-title--active');
+    }
+    for (i of text){        
+        i.classList.remove('main__list-text-container--active');
+    }
+    for (i of arrow){
+        i.classList.remove('main__img-arrow--active');
+    }
+};
+const activar = targetElement => {
+    targetElement.classList.add('main__list-title--active');
+    targetElement.nextElementSibling.classList.add('main__list-text-container--active');
+    targetElement.firstElementChild.classList.add('main__img-arrow--active');
+}
+
+acordion.addEventListener('click' , e => {  
+    if (e.target.classList.contains('main__list-title')){
+        if (e.target.classList.contains('main__list-title--active')){
+            desactivar(e.target);
+        }else {
+            desactivar(e.target);
+            activar(e.target);
+        }    
+    }  
+});
